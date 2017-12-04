300741b3ffd38bfe9c10a3e735bd6adeb53dc310
leighshepperson <leigh.shepperson@gmail.com>
2017-12-04 20:04:19 +0000

    Test and implementation for y get accessor

--- src/modules/ChessSquare.js
+++ src/modules/ChessSquare.js
@@ -1,10 +1,15 @@
 class ChessSquare {
-  constructor(x) {
+  constructor(x, y) {
     this._x = x;
+    this._y = y;
   }
 
   get x() {
     return this._x;
+  }
+
+  get y() {
+    return this._y;
   }
 }
 
--- src/modules/ChessSquare.test.js
+++ src/modules/ChessSquare.test.js
@@ -2,7 +2,12 @@ const ChessSquare = require('./ChessSquare');
 
 describe('ChessSquare', () => {
   test(`should get x position`, () => {
-    const chessSquare = new ChessSquare(3);
+    const chessSquare = new ChessSquare(3, 5);
     expect(chessSquare.x).toEqual(3);
+  });
+
+  test(`should get y position`, () => {
+    const chessSquare = new ChessSquare(3, 5);
+    expect(chessSquare.y).toEqual(5);
   });
 });
