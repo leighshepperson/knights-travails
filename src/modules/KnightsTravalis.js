4e04a4d2e8fb4a5c143fc686a3a4b19add09c325
leighshepperson <leigh.shepperson@gmail.com>
2017-12-04 21:30:02 +0000

    Added implementation for shortest path for a knight

--- src/modules/KnightsTravalis.js
+++ src/modules/KnightsTravalis.js
@@ -1,5 +1,15 @@
 const ChessSquare = require('./ChessSquare');
 
+const getPath = (frontSquare, prevSquareLookup, acc = []) => {
+  const prevSquare = prevSquareLookup[frontSquare];
+  if (prevSquare === Number.NEGATIVE_INFINITY) {
+    return acc;
+  }
+
+  acc.push(prevSquare);
+  return getPath(prevSquare, prevSquareLookup, acc);
+};
+
 class KnightsTravalis {
   nextMoves(squareName) {
     const chessSquare = ChessSquare.parse(squareName);
@@ -21,6 +31,28 @@ class KnightsTravalis {
   }
 
   shortestPath(startSquare, endSquare) {
+    const queue = [];
+    const prevSquareLookup = {};
+    queue.push(startSquare);
+    prevSquareLookup[startSquare] = Number.NEGATIVE_INFINITY;
+
+    while (queue.length > 0) {
+      const frontSquare = queue.shift();
+      if (frontSquare === endSquare) {
+        return getPath(frontSquare, prevSquareLookup);
+      }
+
+      const nextSquares = this.nextMoves(frontSquare);
+
+      for (const nextSquare of nextSquares) {
+        if (prevSquareLookup[nextSquare]) {
+          continue;
+        }
+        queue.push(nextSquare);
+        prevSquareLookup[nextSquare] = frontSquare;
+      }
+    }
+
     return [];
   }
 }
